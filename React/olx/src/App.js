a6839c6233838b214808ded0689b9f4c549f5740
Amal <ken940005@gmail.com>
2023-05-07 19:09:15 +0530

    there are some bug on this

--- React/olx/src/App.js
+++ React/olx/src/App.js
@@ -1,8 +1,20 @@
 
+import About from "./Container/About";
+import Profile from "./Container/Profile";
+import { useState } from 'react';
+import { BrowserRouter as Routes , Route } from "react-router-dom";
+
 function App() {
+  const [state,setState] = useState('');  
   return (
     <div className="App">
-      <h1>Hey</h1>
+      <button onClick = {()=>setState('About')}>About</button>
+      <button onClick = {()=>setState('Profile')}>Profile</button>
+      <Routes>
+        <Route componant={About} path = '/about'/>
+        <Route componant={Profile} path = '/profile'/>
+       
+      </Routes>
     </div>
   );
 }
---
+++ React/olx/src/Container/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Porfile() {
+  return (
+    <div>
+      <h1>Profile of me</h1>
+    </div>
+  )
+}
+
+export default Porfile

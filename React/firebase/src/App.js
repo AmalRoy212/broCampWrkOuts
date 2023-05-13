61b28c1ccc758479d391b04c57c6069da21cec14
Amal <ken940005@gmail.com>
2023-05-13 14:09:47 +0530

    olx clone almost completed

--- React/firebase/src/App.js
+++ React/firebase/src/App.js
@@ -1,14 +1,31 @@
 
-import { Firebase } from "../src/Firebase";
+import firebase from './Firebase'
 
 function App() {
+  function Handle(){
+    //reading the file from the firebase firestore database
+    firebase.firestore().collection('products').get().then(snapshot=>{
+      snapshot.forEach((obj)=>{
+        console.log(obj.data())
+      })
+    });
+    //inserting to the firebase firestore database
+    firebase.firestore().collection('products').add({
+      name:'vivo',
+      price :2000,
+      type : 'mobile',
+      status : 'new'
+    })
+    //deleting a doc from database with the id of the document
+    firebase.firestore().collection('products').doc('dI2t6OfiOVkI7P17wHAD').delete() 
+    //we had a mobie samsung but after this instruction the mobile of sumsung was deleted
+  }
   return (
     <div className="App">
-      <h1>Hey firebase</h1>
-      <button onClick={()=>{
-        
-      }}>Click me</button>
+      <button onClick={Handle}> Click me</button>
+
     </div>
+    
   );
 }
 
--- React/firebase/src/Firebase.js
+++ React/firebase/src/Firebase.js
@@ -1,17 +1,18 @@
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth';
+import 'firebase/compat/firestore'
+import 'firebase/compat/storage'
 
 
- 
-const firebaseConfig = {
-  apiKey: "AIzaSyBOK7x5N5UnjY4TDqndzH7l5tvdNIsWFRc",
-  authDomain: "todo-app-e3cf0.firebaseapp.com",
-  projectId: "todo-app-e3cf0",
-  storageBucket: "todo-app-e3cf0.appspot.com",
-  messagingSenderId: "940016886081",
-  appId: "1:940016886081:web:91686613f16b1b1f8001c0",
-  measurementId: "G-JHPC7TP12K"
+ const firebaseConfig = {
+  apiKey: "AIzaSyBJRewgeg6DX858yZxymeZzy0iG8QPG6Y8",
+  authDomain: "fir-3db24.firebaseapp.com",
+  projectId: "fir-3db24",
+  storageBucket: "fir-3db24.appspot.com",
+  messagingSenderId: "568934042842",
+  appId: "1:568934042842:web:4ada141d4670b8b3e069e3",
+  measurementId: "G-KMPE57W7K0"
 };
-  
-export const Firebase = firebase.initializeApp(firebaseConfig);
+
+export default firebase.initializeApp(firebaseConfig)
   
